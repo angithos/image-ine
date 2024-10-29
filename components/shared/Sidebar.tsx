83748195f0a8709b2084fc7b5cfe80420773d8ee
angith krishna <angithkrishna07@gmail.com>
2024-10-29 13:23:50 +0300

    vercel testing2

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -62,7 +62,7 @@ const Sidebar = () => {
                     </SignedIn>
 
                     <SignedOut>
-                        <Button asChild className='button bg-purple-gradeint bg-cover'>
+                        <Button asChild className='button bg-purple-gradient bg-cover'>
                             <Link href="/sign-in">Login</Link>
                         </Button>
                     </SignedOut>
